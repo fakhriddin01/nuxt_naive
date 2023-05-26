1b16b3a0ad480aa7c3cd7d80719358e7a0b57acd
fakhriddin01 <a.fakhriddin@gmail.com>
2023-05-26 18:04:50 +0500

    1:

---
+++ components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <div class="container mx-auto">
+           <div class="flex">
+            <div class="flex justify-between h-[86px] items-center">
+                    <img src="/logo-colored.svg" alt="">
+            </div>  
+    
+    
+   
+        <n-input-group>
+            
+            <n-input :style="{ width: '50%' }" />
+            <n-button type="primary" ghost>
+              Search
+            </n-button>
+        </n-input-group>
+           </div>
+   
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Navbar/>
+            <main class="min-h-screen">
+                <slot/>
+            </main>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    
+
+</style>
